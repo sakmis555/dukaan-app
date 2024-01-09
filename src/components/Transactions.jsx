918735eaa8e6abc4eb2ee81ca40cb233c1ed63ed
Saksham Mishra <sakmis555@gmail.com>
2024-01-10 00:28:02 +0530

    Pagnation func added

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,29 +1,50 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import orderData from "../data/orderData";
+import Pagination from "./Pagination";
 
+let PageSize = 7;
 const Transactions = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return orderData.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+
   return (
     <div className="transactions">
       <text>Transactions | This Month</text>
       <div>Search , Sort , Download</div>
-      <body>
-        <div className="row heading">
-          <text>Order ID</text>
-          <text>Order Date</text>
-          <text>Order Amount</text>
-          <text>Transaction Fees</text>
-        </div>
-        {orderData.map((order) => {
-          return (
-            <div className="row">
-              <text>{order.order_id}</text>
-              <text>{order.order_date}</text>
-              <text>{order.order_amount}</text>
-              <text>{order.transaction_fees}</text>
-            </div>
-          );
-        })}
-      </body>
+      <table className="table">
+        <thead>
+          <tr className="row heading">
+            <th>Order ID</th>
+            <th>Order Date</th>
+            <th>Order Amount</th>
+            <th>Transaction Fees</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentTableData.map((order) => {
+            return (
+              <tr className="row">
+                <td>{order.order_id}</td>
+                <td>{order.order_date}</td>
+                <td>{order.order_amount}</td>
+                <td>{order.transaction_fees}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={orderData.length}
+        pageSize={PageSize}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
     </div>
   );
 };
