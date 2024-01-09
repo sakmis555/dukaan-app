44465ea5c8ec1e54b8810ecbc5e68b8ac27360f7
Saksham Mishra <sakmis555@gmail.com>
2024-01-09 22:06:46 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      {/*
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+         <img src={logo} className="App-logo" alt="logo" /> 
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+        */}
     </div>
   );
 }
